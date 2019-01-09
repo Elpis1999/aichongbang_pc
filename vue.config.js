39c38162d3ea221578b2a98ab58f9161a1574013
yzx <283232992@qq.com>
2019-01-09 10:22:27 +0800

    基本渲染和传图但是没有传id

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,6 @@
 module.exports = {
     devServer: {
         port: "3000",
-<<<<<<< HEAD
         proxy: 'http://localhost:3001'
-=======
-        proxy:"http://localhost:3001"
->>>>>>> 基本渲染和传图但是没有传id
     }
 }
