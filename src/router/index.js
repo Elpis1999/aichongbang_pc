7307e436b6bae9d10616ebf3d244d4d92d3261f4
yzx <283232992@qq.com>
2019-01-09 11:35:13 +0800

    修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,10 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Manage from "../manage/index.vue";
 import StoreApplication from "../store_manage/store_application/index.vue";
-<<<<<<< HEAD
 import StoreGoods from "../store_manage/goods_manage/index.vue"
 import SupplierGoods from "../store_manage/supplier_goods/index.vue"
 
-=======
 import Suppiler from"../suppiler/index.vue";
->>>>>>> 基本渲染和传图但是没有传id
 Vue.use(Router);
 
 export default new Router({
@@ -19,22 +16,18 @@ export default new Router({
         children: [{
             path: '/manage/storeapplication',
             component: StoreApplication
-<<<<<<< HEAD
-        }
-   , {
+
+        }, {
             path: '/manage/storegoods',
             component: StoreGoods
         }, {
             path: '/manage/suppliergoods',
             component: SupplierGoods
-        }]
-=======
         },
         {
             path: '/manage/suppiler',
             component: Suppiler
-        }
-    ]
->>>>>>> 基本渲染和传图但是没有传id
+        }]
+   
     }]
 })
