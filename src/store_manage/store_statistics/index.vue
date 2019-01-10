b5f821c1bc9175e786092001f0c25c1777ee77a8
zhangye <17628038367@163.com>
2019-01-10 20:19:07 +0800

    增加门店统计

--- src/store_manage/store_statistics/index.vue
+++ src/store_manage/store_statistics/index.vue
@@ -4,11 +4,14 @@
       <el-radio-button label="销量统计"></el-radio-button>
       <el-radio-button label="销售额统计"></el-radio-button>
     </el-radio-group>
+    <Statistics v-if="type === '销量统计'? true : false"/>
+    <SalesVolume v-if="type === '销售额统计'? true : false"/>
   </div>
 </template>
 
 <script>
-import MouthGoodsStatistics from "./MouthGoodsStatistics";
+import Statistics from "./statistics/index";
+import SalesVolume from "./sales_volume/index";
 export default {
   data() {
     return {
@@ -16,7 +19,8 @@ export default {
     };
   },
   components: {
-    MouthGoodsStatistics
+    Statistics,
+    SalesVolume
   }
 };
 </script>
