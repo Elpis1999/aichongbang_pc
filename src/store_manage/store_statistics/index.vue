7bfe1ba7a00228efcb7ca1e51fbdc09aa6d40f08
zhangye <17628038367@163.com>
2019-01-10 17:17:21 +0800

    update

--- src/store_manage/store_statistics/index.vue
+++ src/store_manage/store_statistics/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div style="margin-top: 20px">
     <el-radio-group v-model="type" size="medium">
-      <el-radio-button label="商品销量统计"></el-radio-button>
-      <el-radio-button label="服务销量统计"></el-radio-button>
+      <el-radio-button label="销量统计"></el-radio-button>
+      <el-radio-button label="销售额统计"></el-radio-button>
     </el-radio-group>
   </div>
 </template>
@@ -12,7 +12,7 @@ import MouthGoodsStatistics from "./MouthGoodsStatistics";
 export default {
   data() {
     return {
-      type: "商品销量统计"
+      type: "销量统计"
     };
   },
   components: {
